b9d5ef03d0acff72bcead8c7a7d2d1c09c637af6
yuadh <1912546219@qq.com>
2022-02-03 14:16:08 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -9,21 +9,35 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# deploy to github
-echo 'code.xiaoyou66.com' > CNAME
+# deploy to github pages
+echo 'code.yuadh.com' > CNAME
+
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xiaoyou66/code-learn.git
+  githubUrl=git@github.com:yuadh/code-note.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xiaoyou66:${GITHUB_TOKEN}@github.com/xiaoyou66/code-learn.git
-  git config --global user.name "xiaoyou66"
-  git config --global user.email "xiaoyou2333@foxmail.com"
+  githubUrl=https://yuadh:${GITHUB_TOKEN}@github.com/yuadh/code-note.git
+  git config --global user.name "yuadh"
+  git config --global user.email "1912546219@qq.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
-cd - # 退回开始所在目录
+# deploy to coding pages
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
+
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:xgy/xgy.git
+# else
+#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
+
+cd -
 rm -rf docs/.vuepress/dist
